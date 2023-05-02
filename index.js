f4b75662aa91f628f674d3648afb6a93148695e8
Angela Shen <angelayshen@gmail.com>
2023-05-02 18:45:37 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
+const num1 = 1
+const num2 = 62
+const multiply = num1 * num2
 
-// Write your code here
+const random = Math.ceil(Math.random())
+
+const num3 = 100
+const num4 = 24
+
+const mod = num3 % num4
+
+const max = Math.max(1, 20)
